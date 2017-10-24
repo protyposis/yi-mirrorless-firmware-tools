f56c592bcfda751fa8fade4e2e69b30e7371a533
Mario Guggenberger <mg@protyposis.net>
2017-10-24 00:24:09 -0700

    Parse header

--- index.js
+++ index.js
@@ -22,6 +22,73 @@ fs.open(inputFileName, 'r', (err, fd) => {
             throw err.message;
         }
 
-        console.log(buffer.toString('ascii'));
+        const header = buffer.toString('ascii');
+        console.log(header);
+        parseHeader(header);
     });
 });
+
+function parseHeader(header) {
+    const parsedHeader = {
+        sectionId: undefined,
+        sectionLength: undefined,
+        deviceId: undefined,
+        deviceVersion: undefined,
+        dvr: undefined,
+        sectionSum: undefined,
+        sectionOffset: undefined,
+        followingSectionIds: undefined,
+    };
+
+    const parts = header.split(' ')
+        // Remove empty items (null strings)
+        .filter((part) => part !== '')
+        // Remove whitespace paddings
+        .map((part) => part.trim());
+
+    // Parse the header parts into the parsedHeader structure
+    parts.forEach((part, index) => {
+        const isKeyValue = part.indexOf('=') !== -1;
+        // The first part is a potential section Id
+        if (!isKeyValue && index === 0) {
+            parsedHeader.sectionId = part;
+            return;
+        }
+
+        // After the potential section Id and the length follows the device Id
+        if (!isKeyValue && index < 3) {
+            parsedHeader.deviceId = part;
+            return;
+        }
+
+        if (isKeyValue) {
+            const [ key, value ] = part.split('=');
+
+            switch(key) {
+                case 'LENGTH':
+                    parsedHeader.sectionLength = parseInt(value);
+                    break;
+
+                case 'VER':
+                    parsedHeader.deviceVersion = value;
+                    break;
+
+                case 'DVR':
+                    parsedHeader.dvr = value;
+                    break;
+
+                case 'SUM':
+                    parsedHeader.sectionSum = parseInt(value);
+                    break;
+
+                case 'OFFSET':
+                    parsedHeader.sectionOffset = parseInt(value);
+                    break;
+            }
+        }
+    });
+
+    console.log(parts, parsedHeader);
+
+    return parsedHeader;
+}
